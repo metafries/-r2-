e458f2eb522b72ac789f2fe759a49fff07df9e2b
K.Lin <l714.100@gmail.com>
2018-05-01 23:40:44 +0800

    [180409] Youtube Search API Signup 1.13
    1. add API Key
    2. $ npm install --save youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const YT_DATA_API_V3_KEY = 'AIzaSyBrudJvBV7uhwKnJ-gCiLiPskQiWXaR5Xk';
 const HelloWorld = function() {
     return <div>Hello World!</div>;
 }
