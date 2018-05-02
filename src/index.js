0241ab5b48cbc00774d92efd29218ce2bc50c6b8
K.Lin <l714.100@gmail.com>
2018-05-02 20:59:20 +0800

    [180409] Export Statements 1.14
    add search bar input field

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/search_bar'
 
 const YT_DATA_API_V3_KEY = 'AIzaSyBrudJvBV7uhwKnJ-gCiLiPskQiWXaR5Xk';
-const HelloWorld = function() {
-    return <div>Hello World!</div>;
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
-ReactDOM.render(<HelloWorld />, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
