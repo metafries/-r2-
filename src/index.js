299d6a2749a8be59fef4320a99b31aa4d284ea47
K.Lin <l714.100@gmail.com>
2018-05-05 21:00:42 +0800

    [180409] Youtube Search Response 2.21
    AJAX request for youtube search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
 const YT_DATA_API_V3_KEY = 'AIzaSyBrudJvBV7uhwKnJ-gCiLiPskQiWXaR5Xk';
+YTSearch({key: YT_DATA_API_V3_KEY, term: 'surfboards'}, function(data) {
+    console.log(data)
+});
 const App = () => {
     return (
         <div>
