86f99dc417ec2bd8153de95d211445ccba3dd47c
K.Lin <l714.100@gmail.com>
2018-05-05 23:59:31 +0800

    [180409] Refactoring Functional Components to Class Components 2.22
    change App component to class base

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
 
 const YT_DATA_API_V3_KEY = 'AIzaSyBrudJvBV7uhwKnJ-gCiLiPskQiWXaR5Xk';
-YTSearch({key: YT_DATA_API_V3_KEY, term: 'surfboards'}, function(data) {
-    console.log(data)
-});
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: YT_DATA_API_V3_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+    render () {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );  
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
