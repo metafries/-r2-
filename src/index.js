da5d410b4cd60ab435f90ba791394618e744a7a0
K.Lin <l714.100@gmail.com>
2018-05-09 21:13:42 +0800

    [180409] Real-time search for youtube videos 2.31
    Instantly search for whatever search bar value is

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
-        YTSearch({key: YT_DATA_API_V3_KEY, term: 'ivanontech'}, (videos) => {
+        this.videoSearch('ivanontech');
+    }
+    videoSearch(term) {
+        YTSearch({key: YT_DATA_API_V3_KEY, term: term}, (videos) => {
             this.setState({
                 videos,
                 selectedVideo: videos[0]
@@ -24,7 +27,7 @@ class App extends Component {
     render () {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
