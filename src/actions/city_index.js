e11a129f521a9aa9c394936e6079a3889697ef08
K.Lin <l714.100@gmail.com>
2018-08-30 23:51:47 +0800

    Redux: Middleware: [57] Ajax Requests with Axios
    $ npm install --save axios

--- src/actions/city_index.js
+++ src/actions/city_index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '9e2896b8d57def4f2ff602d27aee841b'
+const ROOT_URL = `https://openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: 'FETCH_WEATHER',
+        payload: request
+    };
+}
