7f2c29ee439bb7a6acccdcf718a44484f2015b15
K.Lin <l714.100@gmail.com>
2018-09-08 20:46:47 +0800

    Redux: Middleware: [67] Google Maps Integration 2/2

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import default from 'axios';
 
 class GoogleMap extends Component {
     componentDidMount() {
-        new GoogleMap.maps.Map(this.refs.map, {
+        new google.maps.Map(this.refs.map, {
             zoom: 12,
             center: {
                 lat: this.props.lat,
